8f61919277bc7c82a90f41480a80ec83284895eb
Matiullah Ansari <you@example.com>
2022-10-17 01:07:02 +0530

    my first commit reactmovie

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from './Components/Main';
+import './Components/style.css';
+import Card from './Components/Card';
+
+const App = () => {
+  return (
+    <>
+    <Main/>
+    
+    </>
+  )
+}
+
+export default App;
